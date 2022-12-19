ef0ce441dbbf6ff287c30ba2511440fd7b072537
Alok Kumar <alok.kumar@manprax.com>
2022-12-19 18:29:17 +0530

    update CustomProfile import details

--- common/djangoapps/student/helpers.py
+++ common/djangoapps/student/helpers.py
@@ -21,7 +21,7 @@ from django.db import IntegrityError, ProgrammingError, transaction
 from django.urls import NoReverseMatch, reverse
 from django.utils.translation import gettext as _
 from pytz import UTC
-from openedx.features.edxplus.mx_accounts.models import CustomUserProfile
+
 
 from common.djangoapps import third_party_auth
 from common.djangoapps.course_modes.models import CourseMode
@@ -737,6 +737,7 @@ def do_create_account(form, custom_form=None):
         "name", "level_of_education", "gender", "mailing_address", "city", "country", "goals",
         "year_of_birth",'mobile_number'
     ]
+    from openedx.features.edxplus.mx_accounts.models import CustomUserProfile
     profile = CustomUserProfile(
         user=user,
         **{key: form.cleaned_data.get(key) for key in profile_fields}
