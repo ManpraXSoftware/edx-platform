667f9a0a31296f08e37ee29ab4a9ddb736ba5791
Om <om.kumar@manprax.com>
2025-01-30 10:46:54 +0530

    fix urls.py

--- lms/urls.py
+++ lms/urls.py
@@ -1054,11 +1054,18 @@ urlpatterns += [
     path('api/notifications/', include('openedx.core.djangoapps.notifications.urls')),
 ]
 
+# from lms.letstalksign import letstalksign_authenticate
+
+# urlpatterns += [
+#         path('letstalksign/authenticate', letstalksign_authenticate, name='letstalksign_api_authenticate_call'),
+
+# ]
+
+from django.urls import re_path
 from lms.letstalksign import letstalksign_authenticate
 
 urlpatterns += [
-        path('letstalksign/authenticate/', letstalksign_authenticate, name='letstalksign_api_authenticate_call'),
-
+    re_path(r'^letstalksign/authenticate/?$', letstalksign_authenticate, name='letstalksign_api_authenticate_call'),
 ]
 # if 'figures' in settings.INSTALLED_APPS:
 #         urlpatterns += (
