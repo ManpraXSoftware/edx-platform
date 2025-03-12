44d685cea2c31cfb69f42808cf45c0f99ff104dc
Sagar <sagar.utlas@manprax.com>
2025-03-12 12:37:34 +0530

    display_image setting in section, subsection and unit task #14914

--- lms/djangoapps/course_api/blocks/transformers/blocks_api.py
+++ lms/djangoapps/course_api/blocks/transformers/blocks_api.py
@@ -54,7 +54,7 @@ class BlocksAPITransformer(BlockStructureTransformer):
         transform method.
         """
         # collect basic xblock fields
-        block_structure.request_xblock_fields('graded', 'format', 'display_name', 'category', 'due', 'show_correctness')
+        block_structure.request_xblock_fields('graded', 'format', 'display_name', 'display_image', 'category', 'due', 'show_correctness')
 
         # collect data from containing transformers
         StudentViewTransformer.collect(block_structure)
