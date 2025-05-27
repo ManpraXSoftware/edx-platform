babc8b4561d77ec57d17fbd4dcf19ab86526f9c1
Om <om.kumar@manprax.com>
2025-05-27 11:28:45 +0530

    updated PROFILE_IMAGE_MAX_BYTES to 5mb

--- lms/envs/common.py
+++ lms/envs/common.py
@@ -4385,7 +4385,7 @@ PROFILE_IMAGE_DEFAULT_FILE_EXTENSION = 'png'
 # profile images. Once it has been set, changing it will make the
 # platform unaware of current image URLs.
 PROFILE_IMAGE_HASH_SEED = 'placeholder_secret_key'
-PROFILE_IMAGE_MAX_BYTES = 1024 * 1024
+PROFILE_IMAGE_MAX_BYTES = 1024 * 1024 * 5 #Manprax
 PROFILE_IMAGE_MIN_BYTES = 100
 PROFILE_IMAGE_SIZES_MAP = {
     'full': 500,
